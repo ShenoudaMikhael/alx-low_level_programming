30bd03da01a553c07f45d2e26e01248ef60f0e6a
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2023-10-05 04:12:02 -0700

    write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
@@ -6,7 +8,7 @@
  */
 int main(void)
 {
-char *outout ="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-printf("%s",outout);
+char *outout = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1, outout, strlen(outout));
 return (1);
 }
