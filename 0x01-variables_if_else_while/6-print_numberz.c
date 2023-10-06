9169035bb638a24162965416d161cced65cdf1a9
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-06 13:22:09 +0300

    no char vars

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+int i;
+i = 48;
+
+while (i < 58)
+{
+/* code */
+
+putchar((char) i);
+i++;
+}
+putchar('\n');
+return (0);
+}
