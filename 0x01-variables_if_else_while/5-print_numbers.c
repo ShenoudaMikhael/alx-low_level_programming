06884ee9ed1167e89a9ab7c87ad52f73ee4ade30
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-06 13:17:58 +0300

    nums only

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+    int i;
+    i = 48;
+
+    while (i < 58)
+    {
+        /* code */
+
+        putchar((char) i);
+        i++;
+    }
+    putchar('\n');
+    return (0);
+}
