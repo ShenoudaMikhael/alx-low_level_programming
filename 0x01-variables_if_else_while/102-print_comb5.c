cb71b9c9fd5a568204722c0e5d28947284690556
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-06 18:19:13 +0300

    betty

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,32 +11,42 @@
  */
 int main(void)
 {
-
-for (int i = 48; i < 58; i++)
+int i, i2, i3, i4;
+i = 48;
+i2 = 48;
+i3 = 48;
+i4 = 48;
+while (i < 58)
+{
+i2 = 48;
+while (i2 < 58)
+{
+i3 = 48;
+while (i3 < 58)
 {
 
-for (int i2 = 48; i2 < 58; i2++)
-{
-for (int i3 = 48; i3 < 58; i3++)
-{
-/*the 4th digit =start with the 2nd digit +1*/
-for (int i4 = i2 + 1; i4 < 58; i4++)
+i4 = i2 + 1;
+while (i4 < 58)
 {
 putchar((char)i);
 putchar((char)i2);
 putchar((char)32);
 putchar((char)i3);
 putchar((char)i4);
-if (i == 57 && i2 == 56 && i3 == 57 && i4 == 57)
+i4++;
+
+if (i == 57 && i2 == 56 && i3 == 57 && i4 == 58)
 {
 continue;
 }
-
 putchar((char)44);
 putchar((char)32);
 }
+i3++;
 }
+i2++;
 }
+i++;
 }
 
 putchar('\n');
