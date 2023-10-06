f433ede12435fc65934ce125ae9cabd6bd9e2b92
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-06 18:23:10 +0300

    test 2

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,44 +11,44 @@
  */
 int main(void)
 {
-int i, i2, i3, i4;
-i = 48;
-i2 = 48;
-i3 = 48;
-i4 = 48;
-while (i < 58)
-{
-i2 = 48;
-while (i2 < 58)
-{
-i3 = 48;
-while (i3 < 58)
-{
+    int i, i2, i3, i4;
+    i = 48;
+    i2 = 48;
+    i3 = 48;
+    i4 = 48;
+    while (i < 58)
+    {
+        i2 = 48;
+        while (i2 < 58)
+        {
+            i3 = 48;
+            while (i3 < 58)
+            {
 
-i4 = i2 + 1;
-while (i4 < 58)
-{
-putchar((char)i);
-putchar((char)i2);
-putchar((char)32);
-putchar((char)i3);
-putchar((char)i4);
-i4++;
+                i4 = 48;
+                while (i4 < 58)
+                {
+                    putchar((char)i);
+                    putchar((char)i2);
+                    putchar((char)32);
+                    putchar((char)i3);
+                    putchar((char)i4);
+                    i4++;
 
-if (i == 57 && i2 == 56 && i3 == 57 && i4 == 58)
-{
-continue;
-}
-putchar((char)44);
-putchar((char)32);
-}
-i3++;
-}
-i2++;
-}
-i++;
-}
+                    if (i == 57 && i2 == 56 && i3 == 57 && i4 == 58)
+                    {
+                        continue;
+                    }
+                    putchar((char)44);
+                    putchar((char)32);
+                }
+                i3++;
+            }
+            i2++;
+        }
+        i++;
+    }
 
-putchar('\n');
-return (0);
+    putchar('\n');
+    return (0);
 }
