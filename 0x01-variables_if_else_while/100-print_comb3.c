de2ec109ce2e32f694f430ceb00d63227f696157
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-06 14:56:24 +0300

    i and i+1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+int i;
+int i2;
+i = 48;
+i2 = 0;
+
+while (i < 58)
+{
+
+i2 = i + 1;
+/* code */
+while (i2 < 58)
+{
+
+/* code */
+putchar((char)i);
+putchar((char)i2);
+if (i != 56)
+{
+putchar((char)32);
+putchar((char)44);
+}
+
+i2++;
+}
+
+i++;
+}
+putchar('\n');
+return (0);
+}
