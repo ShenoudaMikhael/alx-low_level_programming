b5511d67c90f7af5493bd0370b1c12dd9eab6cda
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-06 12:25:18 +0300

    print letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char letters[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    for (size_t i = 0; i < 26; i++)
+    {
+        /* code */
+        putchar(letters[i]);
+    }
+    putchar('\n');
+    return (0);
+}
