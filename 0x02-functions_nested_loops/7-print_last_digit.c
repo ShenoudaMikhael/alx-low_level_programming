d0733c51fb834688df6902024c779cd0b962a229
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-11 01:49:59 +0300

    test3

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,8 +14,14 @@ int print_last_digit(int n)
     if (n < 0)
     {
         n = -1 * n;
+        a = (n % 10);
+        _putchar(a + '0');
     }
-    a = n % 10;
-    _putchar(a + '0');
+    else
+    {
+        a = (n % 10);
+        _putchar(a + '0');
+    }
+
     return (a);
 }
