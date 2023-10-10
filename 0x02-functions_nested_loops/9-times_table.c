a9cd9fcc494e86217b40fa22f05c69b4669f744f
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-11 00:51:26 +0300

    space

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,49 +1,50 @@
-#include "main.h"
-#include <stdio.h>
-/* more headers goes there */
+    #include "main.h"
+    #include <stdio.h>
+    /* more headers goes there */
 
-/* betty style doc for function main goes there */
-/**
- * times_table - Entry point
- *
- * Return: Always void (Success)
- */
-void times_table(void)
-{
-int h1, h2;
-int f;
-int l;
+    /* betty style doc for function main goes there */
+    /**
+     * times_table - Entry point
+     *
+     * Return: Always void (Success)
+     */
+    void times_table(void)
+    {
+    int h1, h2;
+    int f;
+    int l;
 
-h1 = 0;
-while (h1 < 10)
-{
+    h1 = 0;
+    while (h1 < 10)
+    {
 
-h2 = 0;
-while (h2 < 10)
-{
+    h2 = 0;
+    while (h2 < 10)
+    {
 
-l = ((h1)*h2) % 10;
-f = ((h1)*h2) / 10;
-if (f != 0)
-{
-_putchar(f + '0');
-}
-else
-{
-_putchar(' ');
-}
-_putchar(l + '0');
-if (h2 < 9)
-{
-_putchar(',');
-}
+    l = ((h1)*h2) % 10;
+    f = ((h1)*h2) / 10;
+    if (f != 0)
+    {
+    _putchar(f + '0');
+    }
+    else
+    {
+    _putchar(' ');
+    }
+    _putchar(l + '0');
+    if (h2 < 9)
+    {
+    _putchar(',');
+    _putchar(' ');
+    }
 
-h2++;
-}
+    h2++;
+    }
 
-h1++;
-_putchar('\n');
-}
+    h1++;
+    _putchar('\n');
+    }
 
-return;
-}
+    return;
+    }
