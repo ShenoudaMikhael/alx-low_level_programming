7ad62b87ae72a0cb4b959e56822b975e3b0ca9c4
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-11 02:15:46 +0300

    betty

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,33 +9,31 @@
  */
 void print_to_98(int n)
 {
+if(n < 98)
+{
+while (n <= 98)
+{
+printf("%d", n);
+if (n < 98)
+{
+printf(", ");
+}
+n++;
+}
+}
+else
+{
+while (n >= 98)
+{
+printf("%d", n);
+if (n > 98)
+{
+printf(", ");
+}
+n--;
+}
+}
+printf("\n");
 
-    if (n < 98)
-    {
-
-        while (n <= 98)
-        {
-            printf("%d", n);
-            if (n < 98)
-            {
-                printf(", ");
-            }
-            n++;
-        }
-    }
-    else
-    {
-        while (n >= 98)
-        {
-            printf("%d", n);
-            if (n > 98)
-            {
-                printf(", ");
-            }
-            n--;
-        }
-    }
-    printf("\n");
-
-    return;
+return;
 }
