fe537fc2e592f70baaa8faab28c56d2bfd669b91
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-10 20:31:48 +0300

    is alpha ?

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * _isalpha - Entry point
+ * @c: the character to check
+ *
+ * Return: Always Void (Success)
+ */
+int _isalpha(int c)
+{
+
+if ((c >=65 && c <= 90)|| (c >= 97 && c <= 122))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
