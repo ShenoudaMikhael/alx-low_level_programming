a985642085a83777d1ec0026ca8b08efabaa010e
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-11 01:34:56 +0300

    sum no print

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -8,25 +8,8 @@
 int add(int a, int b)
 {
 int sum;
-int f, m, l;
 
 sum = a + b;
 
-f = sum / 100;
-m = (sum / 10) % 10;
-l = sum % 10;
-
-if (f != 0)
-{
-_putchar(f + '0');
-}
-if (f != 0 && m != 0)
-{
-_putchar(m + '0');
-}
-
-_putchar(l + '0');
-_putchar('\n');
-
-return (0);
+return (sum);
 }
