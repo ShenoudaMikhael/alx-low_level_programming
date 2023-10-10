b1621b70c574364d27b9ee4d3efc2131abb1a6ad
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-10 20:48:07 +0300

    -0+

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * _isalpha - Entry point
+ * @c: the character to check
+ *
+ * Return: Always Void (Success)
+ */
+int print_sign(int n)
+{
+
+    if (n > 0)
+    {
+        _putchar(43);
+        return (1);
+    }
+    else if (n < 0)
+    {
+        _putchar('0');
+
+        return (-1);
+    }
+    else
+    {
+        _putchar(45);
+
+        return (0);
+    }
+}
