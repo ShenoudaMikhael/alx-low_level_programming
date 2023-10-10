3ace212c58aa2bdb45f04b8e762bb91400098a9f
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-10 21:04:44 +0300

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * print_sign - Entry point
+ * @n: the character to check
+ *
+ * Return: Always Void (Success)
+ */
+int _abs(int n)
+{
+
+if (n < 0)
+{
+return (n * -1);
+}
+else
+{
+return (n);
+}
+}
