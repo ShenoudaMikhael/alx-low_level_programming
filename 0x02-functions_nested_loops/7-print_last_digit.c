7c0a79e351a45e0005dd63eb840ee4a416791cf7
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-10 21:41:23 +0300

    betty

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * print_last_digit - Entry point
+ * @n: the character to check
+ *
+ * Return: Always Void (Success)
+ */
+int print_last_digit(int n)
+{
+
+return (n % 10);
+}
