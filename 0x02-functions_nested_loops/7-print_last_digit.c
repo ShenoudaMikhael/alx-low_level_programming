0cca24252c9bc921ca0175ea43d8efc9b06b8a8c
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-10 21:49:35 +0300

    last abs digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
@@ -10,6 +11,10 @@
  */
 int print_last_digit(int n)
 {
+if (n < 0)
+{
+n *= -1;
+}
 
 return (n % 10);
 }
