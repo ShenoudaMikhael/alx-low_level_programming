b2708f3ad632ec3ff24170b4bb83f303969885d7
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-11 03:53:09 +0300

    betty

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,29 +8,29 @@
  */
 int main(void)
 {
-    int i = 1;
-    long i2 = 0;
-    long i3 = 1;
-    long sum = 0;
+int i = 1;
+long i2 = 0;
+long i3 = 1;
+long sum = 0;
 
-    while (i < 51)
-    {
-        sum = i2 + i3;
-        i2 = i3;
-        i3 = sum;
-        printf("%ld", sum);
-        if (i == 50)
-        {
-            printf("\n");
-        }
-        else
-        {
-            printf(", ");
-        }
+while (i < 51)
+{
+sum = i2 + i3;
+i2 = i3;
+i3 = sum;
+printf("%ld", sum);
+if (i == 50)
+{
+printf("\n");
+}
+else
+{
+printf(", ");
+}
 
-        i++;
-    }
-    printf("%d,", i);
+i++;
+}
+printf("%d,", i);
 
-    return (0);
+return (0);
 }
