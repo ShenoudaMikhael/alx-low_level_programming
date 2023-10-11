d89206077f227aae10eb4bf1b4806163380bc2f4
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-11 03:00:18 +0300

    test

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -42,7 +42,7 @@ void print_times_table(int n)
             {
                 _putchar(ll + '0');
             }
-            else
+            else if (h1!=0)
             {
                 _putchar(' ');
             }
