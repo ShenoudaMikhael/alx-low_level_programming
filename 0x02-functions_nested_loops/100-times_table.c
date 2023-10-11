c986a224fae1e3d282dc51a69cc4550504b4dd88
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-11 03:23:11 +0300

    test betty

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,50 +10,48 @@
  */
 void print_times_table(int n)
 {
-int c, r;
-if (n < 0 || n > 15)
-{
-return;
-}
-c = 0;
-while (c <= n)
-{
-r = 0;
-while (r <= n)
-{
-int result = c * r;
-
-if (r != 0)
-{
-_putchar(',');
-_putchar(' ');
-}
-if (result < 10)
-{
-if (r != 0)
-{
-_putchar(' ');
-_putchar(' ');
-}
-
-_putchar(result + '0');
-}
-else if (result < 100)
-{
-_putchar(' ');
-_putchar((result / 10) + '0');
-_putchar((result % 10) + '0');
-}
-else
-{
-_putchar((result / 100) + '0');
-_putchar(((result / 10) % 10) + '0');
-_putchar((result % 10) + '0');
-}
-r++;
-}
-_putchar('\n');
-c++;
-}
-return;
+    int c, r, result;
+    if (n < 0 || n > 15)
+    {
+        return;
+    }
+    c = 0;
+    while (c <= n)
+    {
+        r = 0;
+        while (r <= n)
+        {
+            result = c * r;
+            if (result < 10)
+            {
+                if (r != 0)
+                {
+                    _putchar(' ');
+                    _putchar(' ');
+                }
+                _putchar(result + '0');
+            }
+            else if (result < 100)
+            {
+                _putchar(' ');
+                _putchar((result / 10) + '0');
+                _putchar((result % 10) + '0');
+            }
+            else
+            {
+                _putchar((result / 100) + '0');
+                _putchar(((result / 10) % 10) + '0');
+                _putchar((result % 10) + '0');
+            }
+            if (r < n)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+            r++;
+        }
+        _putchar('\n');
+        c++;
+    }
+    return;
 }
