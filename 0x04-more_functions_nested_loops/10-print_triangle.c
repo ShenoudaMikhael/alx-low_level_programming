cc583fdd38b76b827608e99f9429ac20c7366ece
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-13 00:38:22 +0300

    Christmass

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_triangle - print triangle
+ * @size: int tree size
+ * Return: none
+ */
+void print_triangle(int size)
+{
+    int i, i2, i3;
+
+    if (size <= 0)
+    {
+        _putchar('\n');
+        return;
+    }
+
+    for (i = 0; i < size; i++)
+    {
+        for (i2 = i + 1; i2 < size; i2++)
+        {
+            _putchar(' ');
+        }
+        for (i3 = 0; i3 <= i; i3++)
+        {
+            _putchar('#');
+        }
+
+        _putchar('\n');
+    }
+}
