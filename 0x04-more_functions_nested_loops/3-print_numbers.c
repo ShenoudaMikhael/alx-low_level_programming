608fae24a6c0194a406f1962ca5b45270c65adc7
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-12 18:45:41 +0300

    done

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
  * print_numbers - print numbers from 0:9
- * @a: first input
- * @b: second input
- * Return: a *b
+ *
+ * Return: a *b (result)
  */
 void print_numbers(void)
 {
