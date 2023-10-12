baa942bc8acba74ee134a04009b1238f0d172e3a
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-12 19:29:25 +0300

    10*more numbers!

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * more_numbers - print numbers from 0 : 14
+ * Return: None
+ */
+void more_numbers(void)
+{
+	int i, i2, l, r = 0;
+
+	for (i2 = 0; i2 < 10; i2++)
+	{
+		/* code */
+
+		for (i = 0; i < 15; i++)
+		{
+			l = i / 10;
+			r = i % 10;
+
+			if (l != 0)
+			{
+				_putchar(l + '0');
+			}
+			_putchar(r + '0');
+		}
+		_putchar('\n');
+	}
+}
