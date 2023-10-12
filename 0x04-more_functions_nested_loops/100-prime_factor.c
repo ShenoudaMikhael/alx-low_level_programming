94daf0fb8cb21c0cd273d83cb035df0e600a8449
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-13 02:47:04 +0300

    betty

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -5,23 +5,23 @@
  */
 int main(void)
 {
-    long int v;
-    long int i, f;
+long int v;
+long int i, f;
 
-    i = 1;
-    f = 100;
-    v = 612852475143;
+i = 1;
+f = 100;
+v = 612852475143;
 
-    while (i < f)
-    {
-        if (v % i == 0)
-        {
-            f = v / i;
-             
-        }
-       
-        i++;
-    }
-    printf("%ld\n", f);
-    return 0;
+while (i < f)
+{
+if (v % i == 0)
+{
+f = v / i;
+
+}
+
+i++;
+}
+printf("%ld\n", f);
+return 0;
 }
