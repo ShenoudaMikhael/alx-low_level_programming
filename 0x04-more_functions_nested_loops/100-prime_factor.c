831f690d307ab19e074e1a2f5e41b1f36d19d6a9
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-13 02:46:53 +0300

    prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - find greatest prime factor
+ * Return: 0
+ */
+int main(void)
+{
+    long int v;
+    long int i, f;
+
+    i = 1;
+    f = 100;
+    v = 612852475143;
+
+    while (i < f)
+    {
+        if (v % i == 0)
+        {
+            f = v / i;
+             
+        }
+       
+        i++;
+    }
+    printf("%ld\n", f);
+    return 0;
+}
