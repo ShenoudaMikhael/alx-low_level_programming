e87ee8cc3e6ec05d6acd49ad3ca07ab7bb432e6b
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-12 18:43:53 +0300

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers from 0:9
+ * @a: first input
+ * @b: second input
+ * Return: a *b
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+
+return;
+}
