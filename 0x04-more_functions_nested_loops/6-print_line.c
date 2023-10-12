e8c09a887ee00b6016eaa372236d70a0842a4801
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-12 19:46:53 +0300

    n lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - takes n and print line
+ * @n: int of how loong the line
+ *
+ * Return: always 0 (success)
+ */
+void print_line(int n)
+{
+	int i;
+
+	
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
