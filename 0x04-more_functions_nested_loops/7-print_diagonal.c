ea2c83be5e2eba69d1adc076979cc456724409a9
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-12 21:05:01 +0300

    zero condition

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,6 +9,12 @@ void print_diagonal(int n)
 {
 int i, i2;
 
+if (n <= 0)
+{
+_putchar('\n');
+return;
+}
+
 for (i = 0; i < n; i++)
 {
 for (i2 = 0; i2 < i; i2++)
@@ -16,6 +22,6 @@ for (i2 = 0; i2 < i; i2++)
 _putchar(' ');
 }
 _putchar('\\');
-_putchar('\n'); /* code */
+_putchar('\n');
 }
 }
