8c6dcf1a34dd1f960a4b16e70a496a40be679a1d
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-13 04:04:31 +0300

    wrong output

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_number - print int
+ * @n: int to print
+ *
+ * Return: Always 0.
+ */
+void print_number(int n)
+{
+    int i, d, q;
+    q = 10;
+    i = 100000;
+    while (i > 0)
+    {
+
+        if ((n / i <= 9) && (n / i > 0))
+        {
+            d = n / i;
+            _putchar(d + '0');
+
+            while (q < i)
+            {
+                printf("%d\n", (n % q));
+                q *= 10;
+            }
+        }
+
+        i /= 10;
+    }
+    _putchar('\n');
+}
