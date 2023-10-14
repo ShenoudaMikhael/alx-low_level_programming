e9471af0764cb108e4cecc2bc8ac553ad7aae25c
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-14 14:54:41 +0300

    print numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,38 +1,41 @@
 #include "main.h"
 /**
-* print_number - print int
-* @n: int to print
-*
-* Return: none.
-*/
+ * print_number - print int
+ * @n: int to print
+ *
+ * Return: none.
+ */
 void print_number(int n)
 {
-int d, rtl;
 
-if (n < 0)
-{
-_putchar('-');
-n = -n;
-}
+    int d, rtl, n, zero;
 
-if (n == 0)
-{
-_putchar(48);
-return;
-}
+    n = 2102;
+    zero = 0;
+    rtl = 0;
+    while (n > 0)
+    {
+        d = n % 10;
+        if (d == 0 && rtl == 0)
+        {
+            zero++;
+        }
+        rtl = rtl * 10 + d;
+        n = n / 10;
+        printf("d:%d - rtl:%d - n:%d \n", d, rtl, n);
+    }
+    while (rtl > 0)
+    {
+        d = rtl % 10;
+        putchar('0' + d);
+        rtl /= 10;
+    }
+    while (zero > 0)
+    {
+        putchar('0');
+        zero--;
+        /* code */
+    }
 
-rtl = 0;
-while (n > 0)
-{
-d = n % 10;
-rtl = rtl * 10 + d;
-n /= 10;
-}
-
-while (rtl > 0)
-{
-d = rtl % 10;
-_putchar('0' + d);
-rtl /= 10;
-}
+    printf("\n");
 }
