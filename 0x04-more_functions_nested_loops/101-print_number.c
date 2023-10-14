a02e28286631f77acbcd9fdac4b78142219ef549
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-14 16:51:43 +0300

    3

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,89 +1,38 @@
 #include "main.h"
-
 /**
- * settle_zeros - print n zeros
- * @zero: number of zeros to print
- * Return: none
- */
-void settle_zeros(int zero)
-{
-while (zero > 0)
-{
-_putchar('0');
-zero--;
-}
-}
-
-/**
- * rev_print - reverse and print numbers taking
- * in account the limit of int variable
- * @n: the number to print
- * Return: None
- */
-void rev_print(int n)
-{
-int d, rtl, zero, overflow;
-
-while (n > 0)
-{
-d = n % 10;
-if (d == 0 && rtl == 0)
-{
-zero++;
-}
-if (d > 1 && d <= 9 && rtl == 0)
-{
-
-overflow = d;
-n = n / 10;
-rtl = 1;
-continue;
-}
-
-rtl = rtl * 10 + d;
-
-n = n / 10;
-}
-while (rtl > 0)
-{
-
-d = rtl % 10;
-if (overflow > 0 && rtl / 10 == 0)
-{
-d = overflow;
-overflow = 0;
-}
-_putchar('0' + d);
-rtl /= 10;
-}
-settle_zeros(zero);
-}
-
-/**
- * print_number - print int
- * @n: int to print
- *
- * Return: none.
- */
+* print_number - print int
+* @n: int to print
+*
+* Return: none.
+*/
 void print_number(int n)
 {
-int negative;
-negative = 0;
+int d, rtl;
 
 if (n < 0)
 {
-negative = 1;
-n *= -1;
+_putchar('-');
+n = -n;
 }
 
-else if (n == 0)
+if (n == 0)
 {
-_putchar('0');
+_putchar(48);
 return;
 }
-if (negative == 1)
+
+rtl = 0;
+while (n > 0)
 {
-_putchar('-');
+d = n % 10;
+rtl = rtl * 10 + d;
+n /= 10;
 }
-rev_print(n);
+
+while (rtl > 0)
+{
+d = rtl % 10;
+_putchar('0' + d);
+rtl /= 10;
+}
 }
