b635d22849a979f80a8e9bddc3ee697401efa46a
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-14 14:54:50 +0300

    betty

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,34 +8,34 @@
 void print_number(int n)
 {
 
-    int d, rtl, n, zero;
+int d, rtl, n, zero;
 
-    n = 2102;
-    zero = 0;
-    rtl = 0;
-    while (n > 0)
-    {
-        d = n % 10;
-        if (d == 0 && rtl == 0)
-        {
-            zero++;
-        }
-        rtl = rtl * 10 + d;
-        n = n / 10;
-        printf("d:%d - rtl:%d - n:%d \n", d, rtl, n);
-    }
-    while (rtl > 0)
-    {
-        d = rtl % 10;
-        putchar('0' + d);
-        rtl /= 10;
-    }
-    while (zero > 0)
-    {
-        putchar('0');
-        zero--;
-        /* code */
-    }
+n = 2102;
+zero = 0;
+rtl = 0;
+while (n > 0)
+{
+d = n % 10;
+if (d == 0 && rtl == 0)
+{
+zero++;
+}
+rtl = rtl * 10 + d;
+n = n / 10;
+printf("d:%d - rtl:%d - n:%d \n", d, rtl, n);
+}
+while (rtl > 0)
+{
+d = rtl % 10;
+putchar('0' + d);
+rtl /= 10;
+}
+while (zero > 0)
+{
+putchar('0');
+zero--;
+/* code */
+}
 
-    printf("\n");
+printf("\n");
 }
