81ab205f1e97dad269c093708338b93ea4289d89
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-14 16:35:36 +0300

    fixed

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,64 @@
 #include "main.h"
 /**
+ * rev_print - reverse and print numbers taking
+ * in account the limit of int variable
+ * @n: the number to print
+ * Return: None
+ */
+void rev_print(int n)
+{
+int d, rtl, zero, overflow;
+
+while (n > 0)
+{
+d = n % 10;
+if (d == 0 && rtl == 0)
+{
+zero++;
+}
+if (d > 2 && d <= 9 && rtl == 0)
+{
+
+overflow = d;
+n = n / 10;
+rtl = 1;
+continue;
+}
+
+rtl = rtl * 10 + d;
+
+n = n / 10;
+}
+while (rtl > 0)
+{
+
+d = rtl % 10;
+if (overflow > 0 && rtl / 10 == 0)
+{
+d = overflow;
+overflow = 0;
+}
+_putchar('0' + d);
+rtl /= 10;
+}
+settle_zeros(zero);
+}
+
+/**
+ * settle_zeros - print n zeros
+ * @zero: number of zeros to print
+ * Return: none
+ */
+void settle_zeros(int zero)
+{
+while (zero > 0)
+{
+_putchar('0');
+zero--;
+}
+}
+
+/**
  * print_number - print int
  * @n: int to print
  *
@@ -7,16 +66,15 @@
  */
 void print_number(int n)
 {
-int d, rtl, zero, negative;
-
-zero = 0;
-rtl = 0;
+int negative;
 negative = 0;
+
 if (n < 0)
 {
 negative = 1;
 n *= -1;
 }
+
 else if (n == 0)
 {
 _putchar('0');
@@ -26,25 +84,5 @@ if (negative == 1)
 {
 _putchar('-');
 }
-while (n > 0)
-{
-d = n % 10;
-if (d == 0 && rtl == 0)
-{
-zero++;
-}
-rtl = rtl * 10 + d;
-n = n / 10;
-}
-while (rtl != 0)
-{
-d = rtl % 10;
-_putchar('0' + d);
-rtl /= 10;
-}
-while (zero > 0)
-{
-_putchar('0');
-zero--;
-}
+rev_print(n);
 }
