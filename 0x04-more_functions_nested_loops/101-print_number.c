7405d5582bb89f9314a8d8d74c7d94dd2c1f3aef
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-14 15:02:06 +0300

    fix1 betty

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,46 +8,46 @@
 void print_number(int n)
 {
 
-    int d, rtl, zero, negative;
+int d, rtl, zero, negative;
 
-    zero = 0;
-    rtl = 0;
-    negative = 0;
-    if (n < 0)
-    {
-        negative = 1;
-        n *= -1;
-    }
-    else if (n == 0)
-    {
-        _putchar('0');
-        return;
-    }
+zero = 0;
+rtl = 0;
+negative = 0;
+if (n < 0)
+{
+negative = 1;
+n *= -1;
+}
+else if (n == 0)
+{
+_putchar('0');
+return;
+}
 
-    if (negative == 1)
-    {
-        _putchar('-');
-    }
-    while (n > 0)
-    {
-        d = n % 10;
-        if (d == 0 && rtl == 0)
-        {
-            zero++;
-        }
-        rtl = rtl * 10 + d;
-        n = n / 10;
-    }
-    while (rtl > 0)
-    {
-        d = rtl % 10;
-        _putchar('0' + d);
-        rtl /= 10;
-    }
-    while (zero > 0)
-    {
-        _putchar('0');
-        zero--;
-        /* code */
-    }
+if (negative == 1)
+{
+_putchar('-');
+}
+while (n > 0)
+{
+d = n % 10;
+if (d == 0 && rtl == 0)
+{
+zero++;
+}
+rtl = rtl * 10 + d;
+n = n / 10;
+}
+while (rtl > 0)
+{
+d = rtl % 10;
+_putchar('0' + d);
+rtl /= 10;
+}
+while (zero > 0)
+{
+_putchar('0');
+zero--;
+
+}
 }
