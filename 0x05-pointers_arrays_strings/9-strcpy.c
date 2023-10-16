7f85bb9eb5a206023f358f91b26b10c7654926c4
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-16 20:45:13 +0300

    realy ??

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * *_strcpy - copystring
+ * @dest: to copy to
+ * @sec: source vlaue
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+
+    dest = src;
+    return dest;
+}
