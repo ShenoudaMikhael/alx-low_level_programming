d63acc3c9c20d8de248ce1b89153bc0ce2d5ab67
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-16 17:06:53 +0300

    betty

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,30 +6,30 @@
  */
 void rev_string(char *s)
 {
-    int i, j, f;
-    char t;
+int i, j, f;
+char t;
 
-    i = 0;
-    j = 0;
+i = 0;
+j = 0;
 
-    while (s[i] != '\0')
-    {
-        i++;
-    }
-    if (i > 0)
-    {
-        i--;
-    }
-    f = i;
-    while (i != f / 2)
-    {
-        t = s[i];
-        s[i] = s[j];
-        s[j] = t;
+while (s[i] != '\0')
+{
+i++;
+}
+if (i > 0)
+{
+i--;
+}
+f = i;
+while (i != f / 2)
+{
+t = s[i];
+s[i] = s[j];
+s[j] = t;
 
-        j++;
-        i--;
-    }
+j++;
+i--;
+}
 
-    return;
+return;
 }
