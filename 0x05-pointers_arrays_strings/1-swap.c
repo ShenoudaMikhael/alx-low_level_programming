6ad3c692e2963a4a288e912cbbe00b6065154c1a
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-16 09:28:42 +0300

    done

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+/**
+ * swap_int - swap two ints'
+ * @a: input 1
+ * @b: input 2
+ */
+void swap_int(int *a, int *b)
+{
+    int temp;
+    
+    temp = *a;
+    *a = *b;
+    *b = temp;
+    return;
+}
