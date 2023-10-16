985fd4b0c7f9addf937b5294f69d88eef6159883
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-16 15:31:55 +0300

    fix 1 - no betty

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,18 +7,19 @@
  */
 void print_rev(char *s)
 {
-int i;
+    int i;
 
-i = 0;
-while (s[i] != '\0')
-{
-i++;
-}
-while (i >= 0)
-{
-_putchar(s[i]);
-i--;
-}
-_putchar('\0');
-_putchar('\n');
+    i = 0;
+    while (s[i] != '\0')
+    {
+
+        i++;
+    }
+    i--;
+    while (i != -1)
+    {
+        _putchar(s[i]);
+        i--;
+    }
+    _putchar('\n');
 }
