362f0ff1e5c892d010477fb1d6b75bc97c6ecefb
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-16 20:10:37 +0300

    test2

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,26 +7,21 @@
  */
 void puts_half(char *str)
 {
-int i, h;
-h = 0;
-i = 0;
-while (str[i] != '\0')
-{
-i++;
-}
-i--;
-if (i % 2 != 0)
-{
-h = (i / 2) + 1;
-}
-else
-{
-h = (i / 2);
-}
-while (h <= i)
-{
-_putchar(str[h]);
-h++;
-}
-_putchar('\n');
+    int i, h;
+    h = 0;
+    i = 0;
+    while (str[i] != '\0')
+    {
+        i++;
+    }
+    i--;
+    h = (i / 2);
+
+
+    while (h <= i)
+    {
+        _putchar(str[h]);
+        h++;
+    }
+    _putchar('\n');
 }
