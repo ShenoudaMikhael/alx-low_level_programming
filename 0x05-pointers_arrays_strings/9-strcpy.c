e0fb6c9f01aeaf8e059295c4a599c88fbc3879e9
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-16 21:00:24 +0300

    attitude matters!

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * *_strcpy - copystring
  * @dest: to copy to
@@ -7,14 +8,14 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-    int i;
-    i = 0;
-    while (i != '\0')
-    {
-        *(dest+i) = src[i];
-        i++;
-    }
+int i;
+i = 0;
+while (src[i] != '\0')
+{
+*(dest + i) = *(src + i);
 
-    dest[i] = '\0';
-    return dest;
+i++;
+}
+dest[i] = '\0';
+return dest;
 }
