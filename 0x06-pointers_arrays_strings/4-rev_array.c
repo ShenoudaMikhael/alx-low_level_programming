9e9a7faecfa6acadc55d497f28393e003bbbb880
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-19 00:53:30 +0300

    betty

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,15 +7,15 @@
  */
 void reverse_array(int *a, int n)
 {
-    int i, temp;
-    i = n - 1;
+int i, temp;
+i = n - 1;
 
-    while (i >= (n) / 2)
-    {
-        temp = a[i];
-        a[i] = a[(n - 1) - i];
-        a[(n - 1) - i] = temp;
-        i--;
-    }
-    return;
+while (i >= (n) / 2)
+{
+temp = a[i];
+a[i] = a[(n - 1) - i];
+a[(n - 1) - i] = temp;
+i--;
+}
+return;
 }
