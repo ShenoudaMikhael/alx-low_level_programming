629679e34cebf2fee795328bce84f08e4b0797e2
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-18 20:12:00 +0300

    fix 1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,18 +11,18 @@ char *_strcat(char *dest, char *src)
 int i, i2;
 i = 0;
 i2 = 0;
-while (src[i] != '\0')
+while (dest[i] != '\0')
 {
 i++;
 }
 
 while (i2 != '\0')
 {
-src[i] = dest[i2];
-i++;
+dest[i] = src[i2];
 i2++;
+i++;
 }
-src[i] = '\0';
+dest[i] = '\0';
 
-return (src);
+return (dest);
 }
