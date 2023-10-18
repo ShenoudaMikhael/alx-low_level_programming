9d0500599d7bb2c51fd4ce277e3fa6a7b1c5ef80
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-19 00:13:02 +0300

    betty

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,22 +8,22 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-    int i;
-    i = 0;
+int i;
+i = 0;
 
-    while (s1[i] != '\0')
-    {
-        if (s1[i] != s2[i])
-        {
+while (s1[i] != '\0')
+{
+if (s1[i] != s2[i])
+{
 
-            return (s1[i] - s2[i]);
-        }
+return (s1[i] - s2[i]);
+}
 
-        i++;
-    }
-    if (s1[i] == '\0' && s2[i] != '\0')
-    {
-        return (-1 * s2[i]);
-    }
-    return (0);
+i++;
+}
+if (s1[i] == '\0' && s2[i] != '\0')
+{
+return (-1 * s2[i]);
+}
+return (0);
 }
