c087356e6fa8c0fa61d812aa6131119b5afc584c
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-18 23:56:50 +0300

    test

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,14 +9,14 @@ int _strcmp(char *s1, char *s2)
 {
     int i;
     i = 0;
-    int s1c;
-    s1c = 0;
+
     while (s1[i] != '\n')
     {
-        if (s1[i] != s2[i])
+        if (s1[i] != s2[i] )
         {
             return (s1[i] - s2[i]);
         }
+      
         i++;
     }
     return (0);
