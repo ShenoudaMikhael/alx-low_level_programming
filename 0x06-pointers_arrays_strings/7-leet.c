dca60dc7bd38288685667874ca01feb2b56d0c10
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-19 02:17:35 +0300

    test1

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * leet - change string to 1337
+ * @s: string
+ * Return: char
+ */
+char *leet(char *s)
+{
+int i, r;
+char a[5] = {97, 101, 108, 111, 116};
+char b[5] = {52, 51, 48, 55, 49};
+i = 0;
+while (s[i] != '\0')
+{
+for (r = 0; r < 5; r++)
+{
+if (s[i] == a[r] || s[i] == (a[r] - 32))
+{
+
+s[i] = b[r];
+}
+}
+
+i++;
+}
+return (s);
+}
