b95fb48c416d5e8ad8786faf3d9195406138b6da
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-19 01:47:36 +0300

    test 1

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * cap_string - captilize string
+ * @s: string
+ * Return: char
+ */
+char *cap_string(char *s)
+{
+    int i, sep;
+
+    char a[13] = {'\t', ' ', '\n', ',', ';', '.', '!', '?', '\"', '(', ')', '{', '}'};
+    i = 0;
+    while (s[i] != '\0')
+    {
+        if (s[i] >= 97 && s[i] <= 122 && i == 0)
+        {
+            s[i] = s[i] - 32;
+        }
+
+        for (sep = 0; sep < 13; sep++)
+        {
+            if (s[i] == a[sep])
+            {
+                if (s[i + 1] >= 97 && s[i + 1] <= 122)
+                {
+                    s[i + 1] = s[i + 1] - 32;
+                }
+            }
+        }
+        i++;
+    }
+    return (s);
+}
