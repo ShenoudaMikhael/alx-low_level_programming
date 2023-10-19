80c10a1371caed665bb53c5108aea8246d703543
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-19 20:35:37 +0300

    fix

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,26 +6,26 @@
  */
 char *rot13(char *s)
 {
-int i, i2;
-char no[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-char ab[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-i=0;
+    int i, i2;
+    char no[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+    char ab[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    i = 0;
 
-while (s[i] != '\n')
-{
-i2 = 0;
-while (i2 < 27)
-{
-if (s[i] == ab[i2])
-{
-s[i] = no[i2];
-break;
-}
-i2++;
-}
+    while (s[i] != '\n')
+    {
+        i2 = 0;
+        while (s[i2] != '\0')
+        {
+            if (s[i] == ab[i2])
+            {
+                s[i] = no[i2];
+                break;
+            }
+            i2++;
+        }
 
-i++;
-}
+        i++;
+    }
 
-return (s);
+    return (s);
 }
