8ed3b8f8584fd295935cda1ad523cebeb8b9229b
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-20 02:40:28 +0300

    fx2

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,38 +1,53 @@
 #include "main.h"
 /**
-* print_number - print int
-* @n: int to print
-*
-* Return: none.
-*/
+ * print_number - print int
+ * @n: int to print
+ *
+ * Return: none.
+ */
 void print_number(int n)
 {
-int d, rtl;
+    int d, rtl, rtl2;
 
-if (n < 0)
-{
-_putchar('-');
-n = -n;
-}
+    if (n < 0)
+    {
+        _putchar('-');
+        n = -n;
+    }
 
-if (n == 0)
-{
-_putchar(48);
-return;
-}
+    if (n == 0)
+    {
+        _putchar(48);
+        return;
+    }
 
-rtl = 0;
-while (n > 0)
-{
-d = n % 10;
-rtl = rtl * 10 + d;
-n /= 10;
-}
+    rtl = 0;
+    rtl2 = 0;
+    while (n > 0)
+    {
+        d = n % 10;
+        if (rtl > 9999)
+        {
+            rtl2 = rtl2 * 10 + d;
+        }
+        else
+        {
 
-while (rtl > 0)
-{
-d = rtl % 10;
-_putchar('0' + d);
-rtl /= 10;
-}
+            rtl = rtl * 10 + d;
+        }
+        n /= 10;
+    }
+
+    while (rtl > 0)
+    {
+        d = rtl % 10;
+        _putchar('0' + d);
+        rtl /= 10;
+    }
+    while (rtl2 > 0)
+    {
+        d = rtl2 % 10;
+        _putchar('0' + d);
+        rtl2 /= 10;
+    }
 }
