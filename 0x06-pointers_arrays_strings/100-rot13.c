a35c2db2399b82dcf045cdae5cadc0143f4c08b3
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-19 20:31:57 +0300

    test1

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * rot13 - replace by +13 char
+ * @s: string to manipulate
+ * Return: string
+ */
+char *rot13(char *s)
+{
+int i, i2;
+char no[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+char ab[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+i=0;
+
+while (s[i] != '\n')
+{
+i2 = 0;
+while (i2 < 27)
+{
+if (s[i] == ab[i2])
+{
+s[i] = no[i2];
+break;
+}
+i2++;
+}
+
+i++;
+}
+
+return (s);
+}
