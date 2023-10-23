5fb703114e8897c5aa6470927bcfb5f0876040dd
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-10-23 22:29:33 +0300

    check mate

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_chessboard -  prints the chessboard.
+ * @board: 2d array
+ * Return: NULL
+ */
+void print_chessboard(char (*a)[8])
+{
+int i, i2;
+
+i = 0;
+while (i < 8)
+{
+i2 = 0;
+while (i2 < 8)
+{
+_putchar(a[i][i2]);
+i2++;
+}
+_putchar('\n');
+i++;
+}
+}
