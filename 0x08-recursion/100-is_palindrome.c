8a34ff48f558c445de21fc593b977606ab30e0f2
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2023-10-26 01:28:10 +0300

    betty

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,12 +1,12 @@
 /**
- * single_check - check for chars
+ * single_check - check for chars form start against end
  * @len: str length
  * @i: index
  * @s: string ptr
+ * Return: int
  */
 int single_check(int len, int i, char *s)
 {
-
 
 if (i >= len)
 {
@@ -21,9 +21,9 @@ return (single_check(len - 1, (i + 1), s));
 return (0);
 }
 /**
- * ptr_len
- * @s: string ptr
- * return: int
+ * ptr_len - get the ptr len by pushing forward
+ * @ptr: string ptr
+ * Return: int
  */
 int ptr_len(char *ptr)
 {
@@ -35,14 +35,15 @@ return (ptr_len(ptr) + 1);
 return (0);
 }
 /**
- * is_palindrome -
- * @s: ptr
- */
+* is_palindrome - returns 1 if a string is a palindrome and 0 if not.
+* @s: ptr
+* Return: int
+*/
 int is_palindrome(char *s)
 {
 int len, i;
 i = 1; /* ??? */
 /* get length */
 len = ptr_len(s);
-return (single_check(len, i, s-1));
+return (single_check(len, i, s - 1));
 }
