93bc1eeccbc9fa01bc9896fff8502ba2884f98b9
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2023-10-26 16:19:39 +0300

    betty

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,34 +1,44 @@
+/**
+ * cmp - compare twos trings
+ * @s1: string
+ * @s2: string
+ * Return: int
+ */
 int cmp(char *s1, char *s2)
 {
-    if (*s1 == '\0' && *s2 == '\0')
-    {
-        return (1);
-    }
-    if (*s1 == *s2 && *s1 != '\0' && *s2 != '\0')
-    {
-        s1++;
-        s2++;
-        return cmp(s1, s2);
-    }
-    if (*s2 == '*')
-    {
-        if ( *s1 == '\0' && *(s2 + 1) != '\0')
-        {
+if (*s1 == '\0' && *s2 == '\0')
+{
+return (1);
+}
+if (*s1 == *s2 && *s1 != '\0' && *s2 != '\0')
+{
+s1++;
+s2++;
+return (cmp(s1, s2));
+}
+if (*s2 == '*')
+{
+if (*s1 == '\0' && *(s2 + 1) != '\0')
+{
 
-            return (0);
-        }
-        if (cmp(s1, s2 + 1) || cmp(s1 + 1, s2))
-        {
-            return (1);
-        }
-    }
+return (0);
+}
+if (cmp(s1, s2 + 1) || cmp(s1 + 1, s2))
+{
+return (1);
+}
+}
 
-        return (0);
+return (0);
 }
 /**
- *
+ * wildcmp - function that compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 1 if the strings can be considered identical,
+ *  otherwise return 0.
  */
 int wildcmp(char *s1, char *s2)
 {
-    return (cmp(s1, s2));
+return (cmp(s1, s2));
 }
