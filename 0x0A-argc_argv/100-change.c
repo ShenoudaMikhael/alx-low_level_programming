c82d72178d973872f84cb3eae28a8c979e560e80
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2023-10-28 01:03:42 +0200

    change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the all args
+ * @argc: int argument count
+ * @argv: pointer arguments array
+ * Return: 0 success
+ */
+int main(int argc, char const *argv[])
+{
+int total, change;
+total = 0;
+change = 0;
+
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+
+total = atoi(argv[1]);
+if (total < 0)
+{
+printf("0\n");
+return (0);
+}
+while (total > 0)
+{
+
+if (total - 25 >= 0)
+{
+total -= 25;
+change++;
+continue;
+}
+if (total - 10 >= 0)
+{
+total -= 10;
+change++;
+continue;
+}
+if (total - 5 >= 0)
+{
+total -= 5;
+change++;
+continue;
+}
+if (total - 2 >= 0)
+{
+total -= 2;
+change++;
+continue;
+}
+if (total - 1 >= 0)
+{
+total -= 1;
+change++;
+continue;
+}
+}
+
+printf("%d\n", change);
+
+return (0);
+}
