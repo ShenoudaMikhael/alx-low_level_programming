472e4aeb60009a67e36156010882632c3bc6a7bd
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2023-10-27 18:20:48 +0200

    betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * main - prints the command name
+ * @argc: int argument count
+ * @argv: pointer arguments array
+ * Return: 0 success
+*/
 int main(int argc, char const *argv[])
 {
 printf("%s\n", argv[0]);
