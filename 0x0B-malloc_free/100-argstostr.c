bbcac25433c9db4710d84b6e8cf220ab12a35200
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-11-01 00:01:00 +0200

    test1

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * argstostr - concat all argus
+ * @ac: count
+ * @av: arguments
+ * Return: prt char
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, z, y, y2;
+	char *t = NULL;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		z = 0;
+		while (av[i][z])
+		{
+			y++;
+			z++;
+		}
+		y++;
+	}
+	t = malloc((sizeof(char) * y) + 1);
+	y2 = 0;
+	for (i = 0; i < ac; i++)
+	{
+		z = 0;
+		while (av[i][z])
+		{
+			t[y2] = av[i][z];
+			y2++;
+			z++;
+		}
+		t[y2] = '\n';
+
+		y2++;
+	}
+	return (t);
+}
