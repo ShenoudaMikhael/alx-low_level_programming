322e7ee7aa12260989d7e46a26bc97c5d0d06715
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-11-01 02:05:48 +0200

    test1

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+/**
+ * strtow - splits a string into words.
+ * @str: string to be splitted
+ * Return
+ */
+char **strtow(char *str)
+{
+    int i, c, i2;
+    char **t;
+    if (str == NULL || str == "")
+    {
+        return (NULL);
+    }
+    c = 0;
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] != ' ')
+        {
+            c++;
+        }
+    }
+    t = malloc(sizeof(char) * c);
+    if (t == NULL)
+    {
+        return (NULL);
+    }
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] != 32)
+        {
+            *t[i2] = str[i];
+            i2++;
+        }
+    }
+    return (t);
+}
