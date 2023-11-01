65049ff6be82b55f2d9a36e5cb11930cf5bd45b7
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-11-01 02:19:21 +0200

    betty

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -6,32 +6,32 @@
  */
 char **strtow(char *str)
 {
-    int i, c, i2;
-    char **t;
-    if (str == NULL )
-    {
-        return (NULL);
-    }
-    c = 0;
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        if (str[i] != ' ')
-        {
-            c++;
-        }
-    }
-    t = malloc(sizeof(char) * c);
-    if (t == NULL)
-    {
-        return (NULL);
-    }
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        if (str[i] != 32)
-        {
-            *t[i2] = str[i];
-            i2++;
-        }
-    }
-    return (t);
+	int i, c, i2;
+	char **t;
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	c = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != ' ')
+		{
+			c++;
+		}
+	}
+	t = malloc(sizeof(char) * c);
+	if (t == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != 32)
+		{
+			*t[i2] = str[i];
+			i2++;
+		}
+	}
+	return (t);
 }
