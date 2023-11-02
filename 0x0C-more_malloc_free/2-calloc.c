c961e40aa79f7e7e5505d235db438cb454c0e28f
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-11-02 23:06:22 +0200

    test2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -7,13 +7,19 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *t;
+	unsigned int i;
+	char *t;
 
 	if (size == 0 || nmemb == 0)
 		return (NULL);
-
 	t = malloc(nmemb * size);
 	if (t == NULL)
 		return (NULL);
+
+	for (i = 0; i < nmemb * size; i++)
+	{
+		t[i] = 0;
+	}
+
 	return (t);
 }
