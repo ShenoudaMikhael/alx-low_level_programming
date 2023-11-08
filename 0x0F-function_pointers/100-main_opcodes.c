f140f86682894cac05177ab4e348d0969fdfd317
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-11-09 01:54:50 +0200

    test1

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+
+    int i, n;
+
+    void *mainAddress = (void *)main;
+    n = atoi(argv[1]);
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%02x ", ((char *)mainAddress)[i]);
+    }
+
+    return 0;
+}
