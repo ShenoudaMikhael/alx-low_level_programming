bc31e2a33681593e9198fcc98cef42bede8d4405
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-11-09 00:05:04 +0200

    test1

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main entry
+ * @argc: arguments count
+ * @argv: arguments
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+    int res;
+    int num1, num2;
+    char *s;
+
+    if (argc > 4)
+    {
+        return (-1);
+    }
+
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[3]);
+
+    s = argv[2];
+    printf("%s\n", s);
+    res = get_op_func(s)(num1, num2);
+    printf("%d\n", res);
+
+    return (0);
+}
