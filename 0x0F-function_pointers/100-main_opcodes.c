c3d5b1bffa89c699a84e1e7493b225d8698447aa
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-11-09 01:55:30 +0200

    test2

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 
     for (i = 0; i < n; i++)
     {
-        printf("%02x ", ((char *)mainAddress)[i]);
+        printf("%02x", ((char *)mainAddress)[i]);
     }
 
     return 0;
