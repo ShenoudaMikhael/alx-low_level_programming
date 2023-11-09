5f2b16808f40d50a447dede83f103991265865cd
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-11-09 02:30:39 +0200

    fix1

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -12,16 +12,17 @@ int main(int argc, char *argv[])
 	int i, n;
 	void *mainAddress = (void *)main;
 
-	n = atoi(argv[1]);
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return (1);
+		exit(1);
 	}
+	n = atoi(argv[1]);
+
 	if (n < 0)
 	{
 		printf("Error\n");
-		return (2);
+		exit(2);
 	}
 	for (i = 0; i < n; i++)
 	{
