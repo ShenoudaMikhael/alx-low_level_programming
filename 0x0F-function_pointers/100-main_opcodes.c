7a55248a4792faf0314e6899b97337c30a687d54
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-11-09 02:29:27 +0200

    betty

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * main - entry point
+ * @argc: int agument count
+ * @argv: agument
+ * Return: int
+ */
 int main(int argc, char *argv[])
 {
 
-    int i, n;
+	int i, n;
+	void *mainAddress = (void *)main;
 
-    void *mainAddress = (void *)main;
-    n = atoi(argv[1]);
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    if (n < 0)
-    {
-        printf("Error\n");
-        return (2);
-    }
-    for (i = 0; i < n; i++)
-    {
-        printf("%02hhx ", ((char *)mainAddress)[i]);
-    }
-    printf("\n");
-    return 0;
+	n = atoi(argv[1]);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (n < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	for (i = 0; i < n; i++)
+	{
+		printf("%02hhx ", ((char *)mainAddress)[i]);
+	}
+	printf("\n");
+	return (0);
 }
