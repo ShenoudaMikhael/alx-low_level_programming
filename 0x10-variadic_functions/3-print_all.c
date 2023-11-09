b6f7ad7ca7cb74a977b514227bc4938ff605698f
ShenoudaMikhael <Shenouda.Mikhael@gmail.com>
2023-11-09 23:22:28 +0200

    fix format

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,44 @@
+#include "variadic_functions.h"
 #include <stdio.h>
-#include <stdarg.h>
+/**
+ * p_int - print int
+ * @a: vaglist
+ * Return: Nothing
+ */
+void p_int(va_list a)
+{
+	printf("%d", va_arg(a, int));
+}
+/**
+ * p_char - print int
+ * @a: vaglist
+ * Return: Nothing
+ */
+void p_char(va_list a) { printf("%c", va_arg(a, int)); }
+/**
+ * p_float - print int
+ * @a: vaglist
+ * Return: Nothing
+ */
+void p_float(va_list a) { printf("%f", va_arg(a, double)); }
+
+/**
+ * p_str - print int
+ * @a: vaglist
+ * Return: Nothing
+ */
+void p_str(va_list a)
+{
+	char *str = va_arg(a, char *);
+
+	if (str)
+	{
+		printf("%s", str);
+
+		return;
+	}
+	printf("(nil)");
+}
 
 /**
  * print_all - print all args
@@ -9,43 +48,40 @@
 void print_all(const char *const format, ...)
 {
 
+	the_types fmt[] = {
+		{'c', p_char},
+		{'i', p_int},
+		{'f', p_float},
+		{'s', p_str},
+		{'\0', NULL}};
 	unsigned int i, n;
-	char *str, *sc = ", ";
+
+	char *sc = ", ", *s = "";
+
 	va_list args;
 
 	va_start(args, format);
 
-	n = 0;
 	i = 0;
-	while (format[i] != '\0' && format != NULL)
-		i++;
-
-	while (format && format[n])
+	while (format != NULL && format[i] != '\0')
 	{
-		if (n == (i - 1))
+		n = 0;
+		while (fmt[n].t != '\0')
 		{
-			sc = "";
+
+			if (fmt[n].t == format[i])
+			{
+				printf("%s", s);
+				fmt[n].f(args);
+				s = sc;
+			}
+
+			n++;
 		}
-		switch (format[n])
-		{
-		case 'c':
-			printf("%c%s", va_arg(args, int), sc);
-			break;
-		case 'i':
-			printf("%d%s", va_arg(args, int), sc);
-			break;
-		case 'f':
-			printf("%f%s", va_arg(args, double), sc);
-			break;
-		case 's':
-			str = va_arg(args, char *);
-			if (str == NULL)
-				str = "(nil)";
-			printf("%s%s", str, sc);
-			break;
-		}
-		n++;
+
+		i++;
 	}
 	printf("\n");
+
 	va_end(args);
 }
