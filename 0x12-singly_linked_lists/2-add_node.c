fc4725d2daed6ccf3c890126e87ca6fe3aed3c8f
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2023-11-17 00:21:40 +0200

    betty

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,24 +4,26 @@
 #include "lists.h"
 
 /**
- * list_len - print list lenght
- * @h: linked list
+ * add_node - add node to the end
+ * @head: linked list
+ * @str: string
  * Return: lenth of list
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    unsigned int len;
-    list_t *new;
-    new = malloc(sizeof(list_t));
-    new->str = strdup(str);
-    len = 0;
-    while (*str != '\0')
-    {
-        len++;
-        str++;
-    }
-    new->len = len;
-    new->next = *head;
-    *head = new;
-    return (*head);
+	unsigned int len;
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	new->str = strdup(str);
+	len = 0;
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	new->len = len;
+	new->next = *head;
+	*head = new;
+	return (*head);
 }
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -4,18 +4,22 @@
 #include "lists.h"
 
 /**
- * list_len - print list lenght
- * @h: linked list
+ * add_node_end - add node to the end
+ * @head: linked list
+ * @str: string
  * Return: lenth of list
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
 	unsigned int len;
 	list_t *new;
+
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 	new->str = strdup(str);
+	if (!new->str)
+		return (NULL);
 	len = 0;
 	while (*str != '\0')
 	{
