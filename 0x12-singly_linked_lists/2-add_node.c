3de10a3828a6a540f052e06b44894322ec298a91
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2023-11-16 23:56:24 +0200

    test1

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - print list lenght
+ * @h: linked list
+ * Return: lenth of list
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+    unsigned int len;
+    list_t *new;
+    new = malloc(sizeof(head));
+    new->str = strdup(str);
+    len = 0;
+    while (*str != '\0')
+    {
+        len++;
+        str++;
+    }
+    new->len = len;
+    new->next = *head;
+    return (new);
+}
