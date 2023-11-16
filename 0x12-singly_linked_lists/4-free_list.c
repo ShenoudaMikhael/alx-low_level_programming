755b91e3d6c0b39cf58410e721f2c37fa7da24b0
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2023-11-17 01:45:54 +0200

    betty

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,14 +9,14 @@
  */
 void free_list(list_t *head)
 {
-    if (head == NULL)
-    {
-        return;
-    }
+	if (head == NULL)
+	{
+		return;
+	}
 
-    free_list(head->next);
+	free_list(head->next);
 
-    free(head->str);
+	free(head->str);
 
-    free(head);
+	free(head);
 }
