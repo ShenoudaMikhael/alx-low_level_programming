b79fb9ce82d2b29ae71d2fdb7e013dfdc6a980d6
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2023-11-21 02:53:34 +0200

    test1

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * free_list - free linked list memory
+ * @head: linked list ptr
+ * Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+	{
+		return;
+	}
+
+	free_listint(head->next);
+
+	free(head);
+}
