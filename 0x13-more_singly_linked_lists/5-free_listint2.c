2760395bf046d03ec69a91ba1585d550522d5b5e
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2023-11-21 03:14:59 +0200

    betty

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "lists.h"
+/**
+ * free_list2 - free linked list memory
+ * @head: linked list ptr
+ * Return: nothing
+ */
+void free_list2(listint_t *head)
+{
+	if (head == NULL)
+	{
+		return;
+	}
+
+	free_list2(head->next);
+	free(head);
+}
+/**
+ * free_listint2 - free linked list memory
+ * @head: linked list ptr
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	if (head == NULL)
+	{
+		return;
+	}
+
+	free_list2(*head);
+}
