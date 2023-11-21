91351c808db78e6960d1d7df9a0d3e7a964dc8da
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2023-11-21 19:33:10 +0200

    test9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,34 +1,6 @@
 #include "lists.h"
 #include <stdlib.h>
-/**
- * rec4 - recursion over the link
- * and pass to next one
- * @new_element: linked list next element
- * @current_index: current index
- * @new_element_index: new index
- * @temp: ptr to new node
- * @head: ptr to list
- * Return: new node or null
- */
-listint_t *rec4(
-	listint_t *head,
-	int new_element,
-	int current_index, int new_element_index, listint_t *temp)
-{
 
-	if (current_index == new_element_index - 1)
-	{
-		temp->n = new_element;
-		temp->next = head->next;
-		head->next = temp;
-		return (temp);
-	}
-	if (new_element_index > current_index && head->next == NULL)
-		return (NULL);
-	if (head->next != NULL)
-		rec4(head->next, new_element, current_index + 1, new_element_index, temp);
-	return (NULL);
-}
 /**
  * insert_nodeint_at_index - insert node at index
  * @head: ptr to linked list
@@ -38,14 +10,40 @@ listint_t *rec4(
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	int current_index = 0;
-	listint_t *q;
+	listint_t *temp, *hold;
+	unsigned int count = 0;
 
-	q = malloc(sizeof(listint_t));
-	if (q == NULL)
-		return (NULL);
 	if (head == NULL)
+	{
 		return (NULL);
+	}
 
-	return (rec4(*head, n, current_index, idx, q));
+	temp = malloc(sizeof(listint_t));
+	if (temp == NULL)
+	{
+		return (NULL);
+	}
+	temp->n = n;
+	hold = *head;
+
+	if (idx == 0)
+	{
+		temp->next = *head;
+		*head = temp;
+		return (*head);
+	}
+
+	while (hold != NULL)
+	{
+		if (count == idx - 1)
+		{
+			temp->next = hold->next;
+			hold->next = temp;
+		}
+		count++;
+		hold = hold->next;
+	}
+	if (idx > count)
+		return (NULL);
+	return (temp);
 }
