1c142654ad2136821510bd36fd7c286f9ae28adf
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2023-11-24 00:47:53 +0200

    test1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * print_binary - print binary for number
+ * @n: number to print
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int base = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	base = 1 << 30;
+
+	while (base > n)
+	{
+		base >>= 1;
+	}
+
+	while (base > 0)
+	{
+		if (n >= base)
+		{
+			_putchar('1');
+			n -= base;
+		}
+		else
+		{
+			_putchar('0');
+		}
+		base >>= 1;
+	}
+}
