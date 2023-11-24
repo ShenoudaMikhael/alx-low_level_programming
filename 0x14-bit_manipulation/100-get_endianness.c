8830de59532a294f40a09349036c5917ede709bb
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2023-11-24 04:21:56 +0200

    betty

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,9 +1,13 @@
+/**
+ * get_endianness - get endianness
+ * Return: 1 or 0.
+ */
 int get_endianness(void)
 {
 	int o = 1;
 
 	if (*((char *)&o) == 1)
 		return (1);
-	else
-		return (0);
+
+	return (0);
 }
