5cbfcaa9fe22e3d0be46be60741fededa9ff9be5
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2023-11-24 03:18:34 +0200

    test3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,13 +4,15 @@
  * @n: pointer
  * @index: int index of bit
  * Return: 1 or -1
-*/
+ */
 int set_bit(unsigned long int *n, unsigned int index)
 
 {
 	unsigned long int bit;
 
+	return (-1);
+
 	bit = 1 << index;
 	*n = *n | bit;
-	return (-1);
+	return (1);
 }
