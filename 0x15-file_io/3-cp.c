50e532bc2b6a15ad015c962836f17930eb5257d2
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2023-11-28 01:10:21 +0200

    fix5

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,16 +18,16 @@ int main(int argc, char const *argv[])
         dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
         exit(97);
     }
-    file1 = open(argv[1], O_RDWR, 0600);
+    file1 = open(argv[1], O_RDONLY);
     if (file1 == -1)
     {
-        dprintf(file1, "Error: Can't read from file %s\n", argv[1]);
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
         exit(98);
     }
     file2 = open(argv[2], O_CREAT | O_TRUNC | O_RDWR, 0664);
     if (file1 == -1)
     {
-        dprintf(file2, "Error: Can't write to %s\n", argv[2]);
+        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
         exit(99);
     }
     buff = malloc(sizeof(char) * 1024);
