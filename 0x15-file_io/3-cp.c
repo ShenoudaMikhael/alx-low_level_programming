54af8c8b1f0c5c83f0130175cccf28da4b73581e
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2023-11-28 00:58:50 +0200

    fix4

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,7 +24,7 @@ int main(int argc, char const *argv[])
         dprintf(file1, "Error: Can't read from file %s\n", argv[1]);
         exit(98);
     }
-    file2 = open(STDERR_FILENO, O_CREAT | O_TRUNC | O_RDWR, 0664);
+    file2 = open(argv[2], O_CREAT | O_TRUNC | O_RDWR, 0664);
     if (file1 == -1)
     {
         dprintf(file2, "Error: Can't write to %s\n", argv[2]);
@@ -34,8 +34,8 @@ int main(int argc, char const *argv[])
     if (buff == NULL)
         return (-1);
     strlen = read(file1, buff, 1024);
-    write(file2, buff, strlen);
-    if (r == -0)
+    r = write(file2, buff, strlen);
+    if (r == 0)
     {
         dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
         exit(99);
