27f7266f62f9c7e2b36718468a2e31356fabc85e
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2023-11-27 23:22:24 +0200

    betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,7 +2,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-
+/**
+ * create_file - create a file
+ * @filename:	file name
+ * @text_content: text to write
+ * Return: 1 on success
+*/
 int create_file(const char *filename, char *text_content)
 {
 	int f, w, stlen = 0;
