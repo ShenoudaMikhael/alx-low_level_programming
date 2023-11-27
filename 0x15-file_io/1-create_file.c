7d5f365f27c1f1e4a72401650fc88fdcf2da1c36
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2023-11-28 00:37:45 +0200

    test1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,6 +11,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int f, w, stlen = 0;
+	char *buff;
 
 	if (text_content == NULL)
 		text_content = "";
---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+/**
+ * main - copy file
+ * @ac: arg count
+ * @av: args
+ * Return: Always 0.
+ */
+int main(int argc, char const *argv[])
+{
+    int file1, file2, strlen;
+    char *buff;
+
+    if (argc != 3)
+    {
+        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+        exit(97);
+    }
+    file1 = open(argv[1], O_RDWR, 0600);
+    if (file1 == -1)
+    {
+        dprintf(file1, "Error: Can't read from file %s\n", argv[1]);
+        exit(98);
+    }
+    file2 = open(argv[2], O_CREAT | O_TRUNC | O_RDWR, 0666);
+    if (file1 == -1)
+    {
+        dprintf(file2, "Error: Can't write to %s\n", argv[2]);
+        exit(99);
+    }
+    buff = malloc(sizeof(char) * 1024);
+    if (buff == NULL)
+        return (-1);
+    strlen = read(file1, buff, 1024);
+    write(file2, buff, strlen);
+
+    return 0;
+}
