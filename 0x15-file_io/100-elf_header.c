e7c26b30985f995870840bb2c8ef0e0b64d21fd2
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2023-11-28 02:56:40 +0200

    crack2

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,3 +1,7 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * mian - check code
  * @argc: arg count
@@ -6,6 +10,10 @@
  */
 int main(int argc, char const *argv[])
 {
-
+	if (argc != 2)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s <elf_file>\n", argv[0]);
+		exit(98);
+	}
 	return (0);
 }
