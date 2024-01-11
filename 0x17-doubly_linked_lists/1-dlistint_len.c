3359b9be1df987b280707a7a6ce33010fa1c2c89
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-01-11 15:00:28 +0200

    1. List length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -4,17 +4,19 @@
 #include "lists.h"
 /**
  * dlistint_len - get linked list length
+ * @h: linked list adress
+ * Return: len of list
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t i = 0;
+	size_t i = 0;
 
-    while (h != NULL)
-    {
-        i++;
-        if (h->next == NULL)
-            break;
-        h = h->next;
-    }
-    return (i);
+	while (h != NULL)
+	{
+		i++;
+		if (h->next == NULL)
+			break;
+		h = h->next;
+	}
+	return (i);
 }
