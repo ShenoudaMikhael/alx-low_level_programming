828fe81e87e725cbb013ffe705c88a76265c016b
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-01-15 21:33:03 +0200

    test9

--- 0x18-dynamic_libraries/custom_rand.c
+++ 0x18-dynamic_libraries/custom_rand.c
@@ -4,10 +4,10 @@
 /**
  * rand - hack nukbers
  * Return: 0
-*/
+ */
 int rand(void)
 {
-	puts("9 8 10 24 75 - 9");
-	puts("Congratulations, you win the Jackpot!");
-	exit(0);
+	write(STDOUT_FILENO, "9 8 10 24 75 - 9\n", 17);
+	write(STDOUT_FILENO, "Congratulations, you win the Jackpot!\n", 38);
+	exit(EXIT_SUCCESS);
 }
