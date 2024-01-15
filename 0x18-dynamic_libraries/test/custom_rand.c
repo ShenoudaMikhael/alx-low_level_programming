b6925ceaca2d9b3ca1530b78f5d8da7785f7b53b
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-01-15 21:12:44 +0200

    test4

--- 0x18-dynamic_libraries/test/custom_rand.c
+++ 0x18-dynamic_libraries/test/custom_rand.c
@@ -1,10 +1,8 @@
-#include <stdio.h>
-#include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
 int rand()
 {
-    write(1, "9 8 10 24 75 - 9\n", 17);
-    write(1, "Congratulations, you win the Jackpot!\n", 39);
-    exit(0);
+	write(1, "9 8 10 24 75 - 9\n", 17);
+	write(1, "Congratulations, you win the Jackpot!\n", 39);
+	exit(0);
 }
