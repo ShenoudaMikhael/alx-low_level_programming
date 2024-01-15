7f97abfce4c51e5c734efa18f4eeee1da43387fd
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-01-15 21:46:20 +0200

    test17

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -q --output-document=$HOME/rand.so https://github.com/ShenoudaMikhael/alx-low_level_programming/blob/master/0x18-dynamic_libraries/custom_rand.so
-AAaexport fsadfLD_PRELOAD=$HOME/rand.so
+export LD_PRELOAD=$HOME/rand.so
