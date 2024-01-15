2e1c2d77c5b595bde565d45de63c6b967bdfad0a
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-01-15 21:27:47 +0200

    test8

--- 0x18-dynamic_libraries/custom_rand.c
+++ 0x18-dynamic_libraries/custom_rand.c
@@ -1,7 +1,11 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-int rand()
+/**
+ * rand - hack nukbers
+ * Return: 0
+*/
+int rand(void)
 {
 	puts("9 8 10 24 75 - 9");
 	puts("Congratulations, you win the Jackpot!");
