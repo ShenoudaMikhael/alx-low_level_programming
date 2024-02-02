eec40acf922533dedd4ba6fb2376e36da2295c7d
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-02-03 01:33:24 +0200

    3. >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,7 +17,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
-	table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t *));
+	table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * table->size);
 	if (table->array == NULL)
 	{
 		free(table);
