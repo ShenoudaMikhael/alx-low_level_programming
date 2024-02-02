57a1c172d6260994c6f34bd438e1145753a974b4
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-02-02 22:19:28 +0200

    3. >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,31 +2,38 @@
 #include <string.h>
 #include <stdio.h>
 #include "hash_tables.h"
+/**
+ * hash_table_set - insert node in hash table
+ * @ht: hash table
+ * @key: key to insert
+ * @value: new value
+ * Return: int 0 or 1
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned int index;
-    hash_node_t *node;
-    char *newKey = NULL;
+	unsigned int index;
+	hash_node_t *node;
+	char *newKey = NULL;
 
-    if (key == NULL || strcmp(key, "") == 0 || ht == NULL)
-        return (0);
+	if (key == NULL || strcmp(key, "") == 0 || ht == NULL)
+		return (0);
 
-    newKey = strdup(key);
-    index = key_index((const unsigned char *)newKey, ht->size);
-    if (ht->array[index] == NULL)
-    {
-        node = malloc(sizeof(hash_node_t));
-        if (node == NULL)
-            return (0);
+	newKey = strdup(key);
+	index = key_index((const unsigned char *)newKey, ht->size);
+	if (ht->array[index] == NULL)
+	{
+		node = malloc(sizeof(hash_node_t));
+		if (node == NULL)
+			return (0);
 
-        node->key = newKey;
-        node->value = strdup(value);
-        node->next = NULL;
-        ht->array[index] = node;
-    }
-    else
-    {
-        ht->array[index]->value = strdup(value);
-    }
-    return (1);
+		node->key = newKey;
+		node->value = strdup(value);
+		node->next = NULL;
+		ht->array[index] = node;
+	}
+	else
+	{
+		ht->array[index]->value = strdup(value);
+	}
+	return (1);
 }
