7a65a7469989f87c85185045f0fbe11b00debae8
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-02-03 00:14:06 +0200

    3. >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -38,7 +38,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 		if (head->next == NULL)
 		{
-			ht->array[index]->value = strdup(value);
+			ht->array[index]->next = node;
 			return (0);
 		}
 
