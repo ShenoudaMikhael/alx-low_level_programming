0937d543d757ac99c8a0fb63126dceb55a80f927
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-02-02 22:43:42 +0200

    3. >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,29 +11,48 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned int index;
-	hash_node_t *node;
+	unsigned int new_index;
+	hash_node_t *node, *notempty_node;
 	char *newKey = NULL;
 
 	if (key == NULL || strcmp(key, "") == 0 || ht == NULL)
 		return (0);
 
-	newKey = strdup(key);
-	index = key_index((const unsigned char *)newKey, ht->size);
-	if (ht->array[index] == NULL)
-	{
-		node = malloc(sizeof(hash_node_t));
-		if (node == NULL)
-			return (0);
+	node = malloc(sizeof(hash_node_t));
 
-		node->key = newKey;
-		node->value = strdup(value);
-		node->next = NULL;
-		ht->array[index] = node;
-	}
+	if (node == NULL)
+		return (0);
+	node->key = newKey;
+	node->value = strdup(value);
+	node->next = NULL;
+
+	newKey = strdup(key);
+	new_index = key_index((const unsigned char *)newKey, ht->size);
+	if (ht->array[new_index] == NULL)
+		ht->array[new_index] = node;
 	else
 	{
-		ht->array[index]->value = strdup(value);
+		notempty_node = ht->array[new_index];
+		if (strcmp(notempty_node->key, key) == 0) /* on top */
+		{
+			node->next = notempty_node->next;
+			ht->array[new_index] = node;
+			free(notempty_node->key);
+			free(notempty_node->value);
+			free(notempty_node);
+		}
+		while (notempty_node->next != NULL && strcmp(notempty_node->key, key) != 0) /* in the middle */
+			notempty_node = notempty_node->next;
+		if (strcmp(notempty_node->key, key) == 0)
+		{
+			node->next = notempty_node->next->next;
+			free(notempty_node->next->key);
+			free(notempty_node->next->value);
+			free(notempty_node->next);
+			notempty_node->next = node;
+		}
+		else
+			notempty_node->next = node;
 	}
 	return (1);
 }
