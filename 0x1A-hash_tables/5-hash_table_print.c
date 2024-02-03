d749e6b819753794af7e2c16208b24ff4543641c
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-02-03 04:02:18 +0200

    5. >>> print(ht)

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -3,40 +3,42 @@
 #include <stdio.h>
 #include "hash_tables.h"
 /**
- *
+ * hash_table_print - print hash table
+ * @ht: hash table
+ * Return: void
  */
 void hash_table_print(const hash_table_t *ht)
 {
 
-    unsigned int i;
-    int end = 0;
-    hash_node_t *a;
+	unsigned int i;
+	int end = 0;
+	hash_node_t *a;
 
-    if (ht == NULL || ht->array == NULL)
-    {
-        return;
-    }
-    printf("{");
+	if (ht == NULL || ht->array == NULL)
+	{
+		return;
+	}
+	printf("{");
 
-    for (i = 0; i < ht->size; i++)
-    {
+	for (i = 0; i < ht->size; i++)
+	{
 
-        if (ht->array[i] != NULL)
-        {
-            a = ht->array[i];
-            while (a != NULL)
-            {
+		if (ht->array[i] != NULL)
+		{
+			a = ht->array[i];
+			while (a != NULL)
+			{
 
-                if (end == 1)
-                {
-                    printf(", ");
-                }
+				if (end == 1)
+				{
+					printf(", ");
+				}
 
-                printf("'%s': '%s'", a->key, a->value);
-                end = 1;
-                a = a->next;
-            }
-        }
-    }
-    printf("}\n");
+				printf("'%s': '%s'", a->key, a->value);
+				end = 1;
+				a = a->next;
+			}
+		}
+	}
+	printf("}\n");
 }
