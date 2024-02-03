4bc779bf01c828a87de0c82dcf1f3871ea36fd26
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-02-03 04:29:55 +0200

    6. >>> del ht

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * free_list - free linked list memory
+ * @head: linked list ptr
+ * Return: nothing
+ */
+void free_list(hash_node_t *head)
+{
+    if (head == NULL)
+    {
+        return;
+    }
+
+    free_list(head->next);
+    head->next = NULL;
+    free(head->value);
+    free(head->key);
+
+    free(head);
+}
+
+void hash_table_delete(hash_table_t *ht)
+{
+
+    unsigned int i = 0;
+
+    if (ht == NULL)
+        return;
+
+    for (i = 0; i < ht->size; i++)
+    {
+        free_list(ht->array[i]);
+        ht->array[i] = NULL;
+    }
+    free(ht->array);
+    free(ht);
+}
