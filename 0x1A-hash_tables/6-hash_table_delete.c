d3b9ea4905cc792a520427d0acb1564b37e464a1
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-02-03 04:43:27 +0200

    6. >>> del ht

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,32 +9,36 @@
  */
 void free_list(hash_node_t *head)
 {
-    if (head == NULL)
-    {
-        return;
-    }
+	if (head == NULL)
+	{
+		return;
+	}
 
-    free_list(head->next);
-    head->next = NULL;
-    free(head->value);
-    free(head->key);
-    
-    free(head);
+	free_list(head->next);
+	head->next = NULL;
+	free(head->value);
+	free(head->key);
+
+	free(head);
 }
-
+/**
+ * hash_table_delete - free hash table
+ * @ht: hash table
+ * Return: void
+ */
 void hash_table_delete(hash_table_t *ht)
 {
 
-    unsigned int i = 0;
+	unsigned int i = 0;
 
-    if (ht == NULL)
-        return;
+	if (ht == NULL)
+		return;
 
-    for (i = 0; i < ht->size; i++)
-    {
-        free_list(ht->array[i]);
-        ht->array[i] = NULL;
-    }
-    free(ht->array);
-    free(ht);
+	for (i = 0; i < ht->size; i++)
+	{
+		free_list(ht->array[i]);
+		ht->array[i] = NULL;
+	}
+	free(ht->array);
+	free(ht);
 }
