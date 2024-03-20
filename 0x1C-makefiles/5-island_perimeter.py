fd4c3882af55bb9700371035d21f974ebce3182e
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-03-20 23:14:30 +0200

    5. Island Perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""island_perimeter Module"""
+
+
+def island_perimeter(grid):
+    """return island perimeter"""
+    height = len(grid)
+    width = len(grid[0])
+
+    ccc = []
+    for h in range(0, height):
+        for w in range(0, width):
+            if grid[h][w] == 1:
+                ccc.append((h, w))
+
+    print(ccc)
+    p = 0
+    for q in ccc:
+        cp = 4
+        if grid[q[0] - 1][q[1]] == 1:
+            cp -= 1
+        if grid[q[0] + 1][q[1]] == 1:
+            cp -= 1
+        if grid[q[0]][q[1] + 1] == 1:
+            cp -= 1
+        if grid[q[0]][q[1] - 1] == 1:
+            cp -= 1
+        p += cp
+    print(p)
