f6335752a5a6fa05d1bea70ea4a265da05917fbc
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-05-06 02:10:40 +0300

    0. Linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,8 +1,7 @@
 #include "search_algos.h"
 /**
- * linear_search - a function that searches for a
- * value in a sorted array of integers using
- * the Binary search algorithm
+ * linear_search - a function that searches for a value in an array
+ * of integers using the Linear search algorithm
  * @array: array to search
  * @size: array size
  * @value: value to find
